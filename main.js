ae48a5db5455b63a2784e896f53f8929ab3db6eb
chris-hildebrandt <ch.hild15@gmail.com>
2022-07-26 09:45:24 -0600

    morning challenge

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ import {AppState} from 'appstate.js'
 let heroes = [
   new Hero("name", "health", "damage", "level")
 ]
+
+function longestWord(str){
+  let array = str.split(" ")
+  let longest = array[0]
+  array.forEach(word => {
+    if(word.length > longest.length){
+      longest = word
+    }
+    
+  });
+}
